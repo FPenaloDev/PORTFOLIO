131a9f5381762bf8d5aaf2cb66801c342fa3666a
FPDev <francisco.penalo.ferrer@gmail.com>
2025-09-07 12:34:13 +0200

    senda añadido

--- js/functions/repEpub.js
+++
@@ -1,8 +0,0 @@
-export function repEpub(){
-    fetch('./proyectos/repEpub.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('rep_epub').innerHTML = html;
-        })
-        .catch(error => console.error('Error al cargar el archivo HTML:', error));
-}
--- js/proyectos.js
+++ js/proyectos.js
@@ -4,5 +4,11 @@ export const proyectos = [
         description: "Proyecto hecho con HTML,CSS y JavaScript.Este reproductor HTML, presenta una interfaz con controles que permitira subirle un fichero epub y reproducirlo respetando tanto su interactividad como su animacion. En desarrollo",
         image: "https://i.postimg.cc/SN9Ps3ZC/EPUB.png",
         link: "https://fpenalodev.github.io/repEpub/index.html"
+    },
+    {
+        title: "Carta Web Restaurante",
+        description: "Proyecto hecho con HTML,CSS y JavaScript.Carta de Restaurante Interactiva con diferentes recursos como animaciones, interactividad y efectos visuales.",
+        image: "https://raw.githubusercontent.com/FPenaloDev/Senda/refs/heads/main/recursos/portada.webp",
+        link: "https://fpenalodev.github.io/Senda/index.html"
     }
 ];
